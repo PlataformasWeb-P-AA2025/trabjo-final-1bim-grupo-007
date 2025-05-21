ae7d3fdb9f793575ed8f4ca10c7025a0b29b5ea3
nahomi2004 <nahomicabrera2004@gmail.com>
2025-05-21 09:53:46 -0500

    Ingresar datos tonotos

---
+++ ingresar_datos_usuario.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# se importa la clase(s) del
+# archivo genera_tablas
+from genera_data import Usuario, Publicacion, Reaccion
+
+# se importa información del archivo configuracion
+from configuracion import cadena_base_datos
+
+import pandas as pd
+
+# se genera enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine(cadena_base_datos)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Ruta usuarios 
+# /home/nahomi/Downloads/Proyecto-Final-B1/trabjo-final-1bim-grupo-007/DATA/usuarios_red_x.csv
+usuarios_csv = pd.read_csv("DATA/usuarios_red_x.csv")
+
+# print(usuarios_csv.usuario)
+
+# Ingresar datos usuario
+'''
+for u in usuarios_csv.usuario:
+        usuario = Usuario(
+                usuarioNombre = u
+                )
+
+        session.add(usuario)
+        print(usuario)
+'''
+
+
+
+session.commit()
