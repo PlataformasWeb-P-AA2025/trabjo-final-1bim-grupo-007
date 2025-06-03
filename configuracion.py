8b45e715027794282b926929a874e643368c1540
RichrdOr <raortiz20@utpl.edu.ec>
2025-06-03 16:42:41 -0500

    Change file

--- configuracion.py
+++ configuracion.py
@@ -2,4 +2,8 @@
 #
 # cadena conector a la base de datos
 #
-cadena_base_datos = 'sqlite:///RichitoyHumita.db' 
+#cadena_base_datos = 'sqlite:///RichitoyHumita.db' 
+
+#Cadena Postgrest
+cadena_base_datos = 'postgresql://richard:123@localhost:5432/richitoyhumita'
+
